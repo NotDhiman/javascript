933148bd59c1a198fb7512eb5e5f2b6ede30946b
Pranav Dhiman <pranav25dhiman110@gmail.com>
2026-01-19 10:29:52 +0530

    balle balle

---
+++ class 2/script.js
@@ -0,0 +1,28 @@
+// const button=document.querySelector('button')
+// button.classList.add('btn')
+// button.classList.add('o')
+// // button.classList.remove('btn')
+// // button.classList.toggle('btn')
+// function message(){
+//     alert("You have clicked the button")
+// }
+// button.addEventListener('click',message)
+ 
+// const button=document.querySelector('button')
+// button.classList.add('btn')
+const button=document.querySelector('button')
+const button2=document.querySelector('.o')
+function Message(event){ console.log(event.key)
+
+}
+button.addEventListener('keydown',Message)
+function kallu(){
+    button.removeEventListener('click', Message)
+}
+button2.addEventListener('click',kallu)
+const handlesubmit=(event)=>{
+    event.preventDefault()
+    console.log("Form Submitted")
+}
+const  form=document.querySelector('form')
+form.addEventListner('submit',handleSubmit)
